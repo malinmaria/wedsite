0f4569f31b58b0bb1ff0742b23963bcf979e25f1
Malin Heineking <mmheineking@gmail.com>
2014-04-19 14:42:12 -0400

    fixed rsvp and registry link

--- registry.php
+++ registry.php
@@ -2,4 +2,7 @@
 
 <?php include('includes/navbar.php')?>
 
+<p>Find our gift registry at <a href="http://www.bedbathandbeyond.com/store/giftregistry/view_registry_guest.jsp?pwsToken=&eventType=Wedding&registryId=541083803&pwsurl=">Bed Bath and Beyond</a></p>
+
+
 <?php include('includes/footer.php')?>
