5d2d573a5f3c59590c4bbb15266c52b808f8d74c
Malin Heineking <mmheineking@gmail.com>
2014-04-28 23:40:39 -0400

    fuck it we're going live

--- ourstory.php
+++ ourstory.php
@@ -6,9 +6,9 @@
 <article class="boxed">
   <p7>
   It all started in August 2007 when we met on my first day of cross country practice at Youngstown State University. We didn't talk much at first but we still bonded over trying to get through
-  gruling Gorby workouts. We got a lot closer during my second year on the team, he asked for my number and we started talking all the time. Things just clicked with us, he was so easy to talk to 
+  grueling Gorby workouts. We got a lot closer during my second year on the team, he asked for my number and we started talking all the time. Things just clicked with us, he was so easy to talk to 
   and we had so much fun together. We officially started dating on March 7, 2009. So many things happened in the next five years. We graduated college together and are still continuing our education.
-  We moved around to different places, we lived in DC for a summer and then to Pittsburgh for the next year and now we are living in and loving Charleson, SC. 
+  We moved around to different places, we lived in DC for a summer and then to Pittsburgh for the next year and now we are living in and loving Charleson, SC.
   </p7>
 </article>
 
--- pstory.php
+++ pstory.php
@@ -4,4 +4,10 @@
 
 <h1> The Proposal Story </h1>
 
+<article class="boxed">
+<p7>
+Thursday morning December 19th, Dan promised he'd take me to breakfast early before we both went to work for the day. I agreed and thought it would be a great way to start the day but he didn't tell me where we were going to eat. We started heading in the direction of the beach near our apartment so I thought we were going to eat somewhere around there. But we didn't park near any restaurants, we parked near a beach entrance. We got out of the car and started heading onto the beach. The sun still hadn't come up yet so there was a nice morning glow. I was excited because we had never seen the sun rise on the beach even though we'd heard Isle of Palms has the most beautiful sunrises. So I whipped out my camera and started snapping a bunch of pictures as the sun started to rise just above the ocean. I finally put my camera away and was enjoying the view with Dan by my side. Then I was ready to get going to breakfast but Dan didn't seem to want to move. After a few more minutes Dan turned to face me, got down on his knee and was holding a ring in his hand and asked me to marry him! It was so beautiful and unexpected! Needless to say, I did eventually get my breakfast and we called off work for the day. &hearts;
+</p7>
+</article>
+
 <?php include('includes/footer.php')?>
